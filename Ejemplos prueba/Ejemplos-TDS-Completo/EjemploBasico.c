f22feeb9e1c8b678b48f789322fa0f1580e7f2a8
Varo412 <alvarora12@gmail.com>
2020-05-22 16:28:28 +0200

    memoria actualizada

---
+++ Ejemplos prueba/Ejemplos-TDS-Completo/EjemploBasico.c
@@ -0,0 +1,17 @@
+#define cte1 23
+#define cte2 04
+#define cte3 56.34
+
+
+void main ( void )
+{
+	int i1, i2, i3;
+	float r1=-0.4, r2=234e-2, r3=23.498e14;
+	char c1[2], c2[2]="SI";
+	Subrutina0Param();
+	Subrutina1Param( 34 );
+	Subrutina3Param( sd , 87.4 , "hola que \"tal\" estas" );
+	concatenacionStrings = "comilla doble \" dentro" + "comilla simple ' dentro" + "comilla simple ' dentro" + "comilla doble \" dentro" + "comilla doble \" y simple ' dentro" + "comilla simple ' y doble \" dentro";
+	resultado_aritmetico1 = ( -45 + entero1 ) * entero2 - entero3 / entero4;
+	otro_resultado_aritmetico = ( 123.456 * -00.69 + 45.07000 ) / ( -123.456 + Funcion2Param( Funcion1Param( 34.2 ) , 34 ) * 123E456 ) + ( -64e-77 * -045e6 - 003E-35 ) * 1.23E4 + -000.64E-77 / -045.0e16 - 0.03E-35;
+}
